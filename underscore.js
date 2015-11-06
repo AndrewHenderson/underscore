a1b7c9548a0a3656462ce6d1a9ef650eb3774de6
andrewhenderson <andrew.m.henderson@gmail.com>
2015-11-06 01:01:20 -0800

    Bugfix in _.deepFind

--- underscore.js
+++ underscore.js
@@ -247,8 +247,12 @@
             }
             result = _.deepFind(_obj, predicate);
           });
-        } else {
+        } else if (_.isObject(obj[i])) {
           result = _.deepFind(obj[i], predicate);
+        } else {
+          if (predicate(obj[i])) {
+            result = obj[i];
+          }
         }
         if (result) {
           return result;
